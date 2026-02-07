fb4f77f0a02e13b7fd17ba562ea5dbb0d4069960
Prashanith <prashanith007@gmail.com>
2026-02-07 17:04:35 +0530

    build_fixes

--- lib/src/services/notification_service.dart
+++ lib/src/services/notification_service.dart
@@ -1,6 +1,3 @@
-import 'dart:math';
-import 'dart:nativewrappers/_internal/vm/lib/typed_data_patch.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
